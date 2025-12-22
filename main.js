7e39d0c6d381ab7911898d281a146cdc24683ee2
andysantiagopr <andysantiagopr@gmail.com>
2025-12-22 09:15:36 -0800

    Fixed the function to add invalid output to work correctly.

--- main.js
+++ main.js
@@ -13,28 +13,35 @@
         ['Sun', 27.9] 
     ];
     
-    const selectEl = document.getElementById("planets"); //Best practice to keep this variable outside the loop. 
+    const selectEl = document.getElementById("planets"); //Best to keep outside the loop if I want to add other functionality later on.
 
+    //Dropdown menu// 
     planets.forEach(planet => { //.forEach will loop through each of the elements in planet. This case it would call the array inside the array. 
         const name = planet[0]; //This makes sure the planet's name is used [0].
         const optionEl = document.createElement("option"); //Creates an element for the HTML file, in this case an option which would go under the select using the .appendChild later. 
-        optionEl.value = name; //This is what JS receives.
+        optionEl.value = name; //This is the value you are giving JS internally.
         optionEl.textContent = name; //This is what the user sees. 
         selectEl.appendChild(optionEl); //Makes the option fall under select in HTML.
     });
 
 
+    //Calculator Function//
     function calculateWeight(weight, planetName) { 
-        for (let planet of planets) {
-            if (planet[0] === planetName) {
-                return planet[1] * weight;
-            }
-        } return "Invalid Input!"
-    } 
+            for (let planet of planets) {
+                if (planet[0] === planetName) {
+                    return planet[1] * weight;
+                }
+        } } 
+
     function handleClickEvent(e) {
         let userWeight = Number(document.getElementById("user-weight").value);
         let planetName = document.getElementById("planets").value; 
         let result = calculateWeight(userWeight, planetName); 
-        document.getElementById("output").textContent = `If you were on ${planetName}, you would weigh ${result.toFixed(2)}lbs!`;
+
+        if (!isNaN(userWeight)) {
+        document.getElementById("output").textContent = `If you were on ${planetName}, you would weigh ${result.toFixed(2)}lbs!`;}
+        else {
+        document.getElementById("output").textContent = 'Invalid input';
+        }
     } 
         document.getElementById("calculate-button").onclick = handleClickEvent;
